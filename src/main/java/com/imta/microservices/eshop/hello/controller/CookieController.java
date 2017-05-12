5750174c7e9241b17d8e92751b7ae45871600b9b
Damien RAYMOND <dam.ray49@gmail.com>
2017-05-12 21:56:37 +0200

    Improve CookieController : add a method to set a cookie and a method to test if a cookie is present

--- src/main/java/com/imta/microservices/eshop/hello/controller/CookieController.java
+++ src/main/java/com/imta/microservices/eshop/hello/controller/CookieController.java
@@ -2,6 +2,7 @@ package com.imta.microservices.eshop.hello.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,11 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by damien on 12/05/2017.
@@ -17,10 +23,32 @@ import java.io.IOException;
 @RestController
 public class CookieController {
 
+    private final HashMap<String, Integer> cpt = new HashMap<>();
+
     @RequestMapping(value = "/setCookie", method = RequestMethod.GET)
     public String setCookie(HttpServletResponse response) throws IOException {
 
-        response.addCookie(new Cookie("cookie_test", "cookie_test_value"));
+        final String value = UUID.randomUUID().toString();
+        response.addCookie(new Cookie("cookie_test", value));
+
+
+        cpt.put(value, 0);
+
+        return "Ok";
+    }
+
+
+    @RequestMapping(value = "/testCookie", method = RequestMethod.GET)
+    public String testCookie(@CookieValue(value = "cookie_test", required=false) String cookie) throws IOException {
+        System.out.println(cookie);
+
+        synchronized (cpt) {
+            cpt.put(cookie, cpt.getOrDefault(cookie, 0) + 1);
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println(cpt);
 
         return "Ok";
     }
